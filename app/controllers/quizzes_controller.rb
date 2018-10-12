8a28965bc76b4231f1f88f7f723ab4880227142b
EC2 Default User <ec2-user@ip-172-31-9-120.us-east-2.compute.internal>
2018-10-12 18:42:12 +0000

    index ランダムで問題を表示

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,12 +1,25 @@
 class QuizzesController < ApplicationController
   
   def index
+    @quizzes = Quiz.order("RANDOM()").first
   end
   
   def new
+    @quiz = Quiz.new
   end
   
   def create
+    @quiz = Quiz.new(params_quiz)
+    @quiz.save
+    redirect_to "/quizzes"
   end
+
+
+  private
   
+  def params_quiz
+    params.permit(:question, :selection, :answer)
+  end
+
+
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
+    
     validates :question, presence: true, length: { in: 1..500 }
     validates :selection, presence: true, length: { in: 1..500 }
     validates :answer, presence: true, length: { in: 1..500 }
