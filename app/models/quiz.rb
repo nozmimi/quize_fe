a1d4bc8231c2d7a32848da083412ca9211a6578e
EC2 Default User <ec2-user@ip-172-31-9-120.us-east-2.compute.internal>
2018-10-13 14:59:00 +0000

    error

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,5 +2,8 @@ class Quiz < ApplicationRecord
     
     validates :question, presence: true, length: { in: 1..1000 }
     validates :selection, presence: true, length: { in: 1..1000 }
+    validates :selection2, presence: true, length: { in: 1..1000 }
+    validates :selection3, presence: true, length: { in: 1..1000 }
+    validates :selection4, presence: true, length: { in: 1..1000 }
     validates :answer, presence: true, length: { in: 1..1000 }
 end
